889a785d2d286f7bbb0062b3aff831b52b60033b
Amrutha <amruthaur99@gmail.com>
2024-08-21 13:15:18 +0400

    css

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -5,8 +5,18 @@
         @include('components.sidebar')
   <!-- Your other content or sections can go here -->
   <main class="main-content">
+      <div class="row">
+            <div class="col-md-8">
             <h2>Main Content Area</h2>
             <p>This is where your main content goes.</p>
+            </div>
+            <div class="col-md-4">
+                  <div class="open-bets">
+                        <h2>Open Bets</h2>
+                  </div>
+            </div>
+      </div>
+           
  </main>
  </div>
 @endsection
