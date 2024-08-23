2baf8aace55d0a2aad908ae60fa6fe52cd5d8657
Amrutha <amruthaur99@gmail.com>
2024-08-23 10:11:34 +0400

    event-deatils

--- resources/views/event_details.blade.php
+++ resources/views/event_details.blade.php
@@ -81,6 +81,7 @@
                             @endif
                         </div>
                     @endif
+                    <!-- BookMakers -->
                     @if(isset($event_details['data']['event']['book_makers']))
                         @foreach($event_details['data']['event']['book_makers'] as $bookmakers)
                             <div class="dScreen book_makers">
@@ -124,79 +125,35 @@
                                             </button>
                                         </div>
                                     </div>
-                          
-                            </div>
-                        @endforeach
-                    @endif
-                    @if(isset($event_details['data']['event']['book_makers']))
-                        @foreach($event_details['data']['event']['book_makers'] as $bookmakers)
-                        @endforeach
-                    <!-- <div class="sr-widget-1"></div> -->
-                        <div class="dScreen book_makers">
-                            <div class="row mx-0 head_bg">
-                                <div class="col-md-12 col-8 px-0">
-                                    <p class="match-odds">{{ $event_details['data']['event']['match_odds']['market_name']}} 
-                                        <a  href="javascript:void(0)">
-                                            <i  class="mdi mdi-information-outline"></i>
-                                        </a>
-                                    </p>
                                 </div>
-                                <div  class="col-md-6 col-4 text-end px-0 d-inline-block d-lg-none">
-                                    <a  href="javascript:void(0)" class="btn btn-tv-bets mob_open_bets">
-                                        <span>Bets</span>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="row mx-0 odds_header">
-                                <div class="col-md-5 col-7 px-0">
-                                    <div  class="minmax mm-fi">
-                                        <dl class="fancy-info">
-                                            <dt>Min/Max</dt>
-                                            <dd>100-10k</dd>
-                                        </dl>
-                                    </div>
-                                </div>
-                                <div class="col-md-7 col-5 px-0">
-                                    <div  class="btn-group dOddsBox">
-                                        <button class="back2"></button>
-                                        <button class="back1"></button>
-                                        <button class="back">back</button>
-                                        <button class="lay">lay</button>
-                                        <button class="min-max-bet">
-                                            <dl class="fancy-info">
-                                                <dt>Min/Max</dt>
-                                                <dd>100-10k</dd>
-                                            </dl>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            @if(isset($event_details['data']['event']['match_odds']['runners']))                           
-                                @foreach($event_details['data']['event']['match_odds']['runners'] as $runners)
-                                    <div class="row mx-0 odds_body">          
-                                        <div class="col-md-5 col-7 px-0">
-                                            <p class="team-name">{{$runners['name'] }}</p>
-                                        </div>
-                                        <div class="col-md-7 col-5 px-0">
-                                            <div class="btn-group dOddsBox">
-                                                <button class="back back2">1.55 <span >10381.37</span></button>
-                                                <button  class="back back1">1.56 <span >2574.72</span></button>
-                                                <button  class="back">1.58 <span >7032.4</span></button>
-                                                <button  class="lay">1.59 <span >7875.09</span></button>
-                                                <button  class="lay lay1">1.6 <span >1658.19</span></button>
-                                                <button  class="lay lay2">1.61 <span >846.13</span></button>
+                                @if(isset($event_details['data']['event']['match_odds']['runners']))                           
+                                    @foreach($event_details['data']['event']['match_odds']['runners'] as $runners)
+                                        <div class="row mx-0 odds_body">          
+                                            <div class="col-md-5 col-7 px-0">
+                                                <p class="team-name">{{$runners['name'] }}</p>
+                                            </div>
+                                            <div class="col-md-7 col-5 px-0">
+                                                <div class="btn-group dOddsBox">
+                                                    <button class="back back2">1.55 <span >10381.37</span></button>
+                                                    <button  class="back back1">1.56 <span >2574.72</span></button>
+                                                    <button  class="back">1.58 <span >7032.4</span></button>
+                                                    <button  class="lay">1.59 <span >7875.09</span></button>
+                                                    <button  class="lay lay1">1.6 <span >1658.19</span></button>
+                                                    <button  class="lay lay2">1.61 <span >846.13</span></button>
+                                                </div>
                                             </div>
                                         </div>
+                                    @endforeach
+                                    <div class="row mx-0 odds_body">
+                                        <div class="fancy_message">
+                                            <span>{{ $event_details['data']['event']['match_odds']['message']}}</span>
+                                        </div>
                                     </div>
-                                @endforeach
-                                <div class="row mx-0 odds_body">
-                                    <div class="fancy_message">
-                                        <span>{{ $event_details['data']['event']['match_odds']['message']}}</span>
-                                    </div>
-                                </div>
-                            @endif
-                        </div>
+                                @endif
+                            </div>
+                        @endforeach
                     @endif
+                    <!-- BookMakers -->
                 @endif
             @endif 
         </div>  
